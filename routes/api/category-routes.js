9cb3ed49bdfd5d192e584989021c81e8cc77ea8a
Christine Nguyen <cnhnguyen59@gmail.com>
2022-02-06 19:55:36 -0500

    created all api routes for category table

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -36,14 +36,55 @@ router.get('/:id', async (req, res) => {
 
 router.post('/', async(req, res) => {
   // create a new category
+  try {
+    const newCategory = await Category.create({
+      category_name: req.body.category_name,
+    });
+    res.status(200).json(newCategory);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', async(req, res) => {
   // update a category by its `id` value
+  try {
+    const categoriesData = await Category.update(
+    {
+      category_name: req.body.category_name
+    },
+    {
+      where:{
+        id: req.params.id,
+      },
+    });
+
+    if(!categoriesData){
+      res.status(400).json({message:'No category is associated with that id'})
+    }
+
+    res.status(200).json(categoriesData)
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', async(req, res) => {
   // delete a category by its `id` value
+  try {
+    const categoriesData = await Category.destroy({
+      where: {
+        id: req.params.id
+      },
+    });
+
+    if(!categoriesData){
+      res.status(400).json({message: 'No category is associated with that id'})
+    }
+    res.status(200).json(categoriesData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
